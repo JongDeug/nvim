cc8bda82e3c97e6cddff11487b707345e0508740
JongDeug <jongdeug2021@gmail.com>
2024-09-10 17:02:50 +0900

    change tsserver to ts_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "svelte" },
+				ensure_installed = { "lua_ls", "ts_ls", "svelte" },
 			})
 		end,
 	},
