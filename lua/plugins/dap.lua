a1d56c6ef9544321ef048d9bfeb73d34889443a3
JongDeug <jongdeug2021@gmail.com>
2024-09-11 16:58:50 +0900

    dap javascript 디버깅 추가

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,69 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		lazy = true,
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"mxsdev/nvim-dap-vscode-js",
+		},
+	},
+
+	{
+		"mxsdev/nvim-dap-vscode-js",
+		dependencies = {
+			"microsoft/vscode-js-debug",
+		},
+		config = function()
+			require("dap-vscode-js").setup({
+				debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+				adapters = { "pwa-node", "node-terminal" },
+			})
+
+			for _, language in ipairs({ "typescript", "javascript" }) do
+				require("dap").configurations[language] = {
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-node",
+						request = "attach",
+						name = "Attach",
+						processId = require("dap.utils").pick_process,
+						cwd = "${workspaceFolder}",
+					},
+				}
+			end
+		end,
+	},
+
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open({})
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close({})
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close({})
+			end
+		end,
+	},
+
+	{
+		"microsoft/vscode-js-debug",
+		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+		lazy = true,
+	},
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,35 +25,6 @@ return {
 			lspconfig.lua_ls.setup({})
 			lspconfig.ts_ls.setup({})
 			lspconfig.svelte.setup({})
-
-			mapKey("K", vim.lsp.buf.hover)
-			mapKey("gd", vim.lsp.buf.definition)
-			mapKey("<leader>ca", vim.lsp.buf.code_action)
-		end,
-	},
-
-	{
-		"mfussenegger/nvim-dap",
-		lazy = true,
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-		},
-		keys = {
-			{
-				"<leader>d",
-				function()
-					require("dap").toggle_breakpoint()
-				end,
-			},
-			{
-				"<leader>c",
-				function()
-					require("dap").continue()
-				end,
-			},
-		},
-		config = function()
-			require("dapui").setup()
 		end,
 	},
 }
