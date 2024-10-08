de360d7eb27f7028d01a9a171d845dd9317189dc
JongDeug <jongdeug2021@gmail.com>
2024-10-08 12:35:42 +0900

    vscode-js-debug dependencies 제거

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,9 +10,9 @@ return {
 
 	{
 		"mxsdev/nvim-dap-vscode-js",
-		dependencies = {
-			"microsoft/vscode-js-debug",
-		},
+		-- dependencies = {
+		-- 	"microsoft/vscode-js-debug",
+		-- },
 		config = function()
 			require("dap-vscode-js").setup({
 				debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
